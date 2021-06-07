13e1feec821d7543b07dc48f574ed2e77bc36247
Adrian Saiz <adrian.saiz@gmail.com>
2021-06-07 11:37:59 +0200

    v Version 1

---
+++ prueba/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace prueba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Este es el archivo 1");
+
+
+
+        }
+    }
+}
---
+++ prueba2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace prueba2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Wooooorld!");
+            Console.WriteLine("ESTE ES EL ARCHIVO 2");
+        }
+    }
+}
